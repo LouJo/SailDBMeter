42814d1e6c312afba725554f2568093818fa8d64
Louis-Joseph Fournier <louisjoseph.fournier@gmail.com>
2015-11-14 18:10:24 +0100

    Quit signal handled for sailfish

--- src/sailfish.cpp
+++ src/sailfish.cpp
@@ -4,14 +4,30 @@
 #include <sailfishapp.h>
 #include "DBMeter.hpp"
 
+class Main {
+	private:
+	QGuiApplication *app;
+	QQuickView *view;
+
+	public:
+	Main(int &argc, char* argv[]) {
+		app = SailfishApp::application(argc, argv);
+		view = SailfishApp::createView();
+		view->setSource(QUrl("qrc:///qml/Sailfish.qml"));
+		QObject::connect(view->engine(), SIGNAL(quit()), app, SLOT(quit()));
+	}
+	Main() {}
+	~Main() {}
+
+	int Launch() {
+		view->show();
+		return app->exec();
+	}
+};
+
 Q_DECL_EXPORT int main(int argc, char* argv[])
 {
 	qmlRegisterType<DBMeter>("LJDBMeter", 1, 0, "DBMeter");
-
-	QGuiApplication *app = SailfishApp::application(argc, argv);
-	QQuickView *view = SailfishApp::createView();
-	view->setSource(QUrl("qrc:///qml/Sailfish.qml"));
-	view->show();
-
-	return app->exec();
+	Main *appli = new Main(argc, argv);
+	return appli->Launch();
 }
